54e6553c6305fb1bec79f277890b165388aee2f4
unknown <juiceppe19@gmail.com>
2020-09-30 00:34:37 +0200

    Added databaseService and moved stuff in order to have proper structure

--- lib/services/authService.dart
+++ lib/services/authService.dart
@@ -1,14 +1,26 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:wildnature/services/databaseService.dart';
 
 class AuthenticationService {
+  final DateTime userCreatedDate = DateTime.now();
   final FirebaseAuth _firebaseAuth;
 
   AuthenticationService(this._firebaseAuth);
 
   Stream<User> get authStateChanges => _firebaseAuth.authStateChanges();
 
-  Future<void> signOut() async {
-    await _firebaseAuth.signOut();
+  Future<String> signUp(
+      {String fullName, String email, String password}) async {
+    try {
+      UserCredential result = await _firebaseAuth
+          .createUserWithEmailAndPassword(email: email, password: password);
+      User user = result.user;
+      await DatabaseService(uid: user.uid)
+          .updateUser(fullName, userCreatedDate);
+      return "Signed up";
+    } on FirebaseAuthException catch (e) {
+      return e.message;
+    }
   }
 
   Future<String> signIn({String email, String password}) async {
@@ -21,13 +33,7 @@ class AuthenticationService {
     }
   }
 
-  Future<String> signUp({String email, String password}) async {
-    try {
-      await _firebaseAuth.createUserWithEmailAndPassword(
-          email: email, password: password);
-      return "Signed up";
-    } on FirebaseAuthException catch (e) {
-      return e.message;
-    }
+  Future<void> signOut() async {
+    await _firebaseAuth.signOut();
   }
 }
